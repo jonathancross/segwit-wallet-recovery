6b882f56012b99e0728da0c79cebf18f524af950
madacol <madacol10@gmail.com>
2018-10-31 20:20:43 -0500

    Feat: Test multiple addresses with multiple generated addresses

--- test-pw.py
+++ test-pw.py
@@ -1,49 +1,50 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 import sys
 from lib.keystore import from_bip39_seed
 from lib.storage import WalletStorage
 from lib.wallet import Standard_Wallet
 
+# Change this to be YOUR seed phrase:
+SEED_WORDS = 'final round trust era topic march brain envelope spoon minimum bunker start'
+# Change this to be the addresses the wallet might had.
+POSSIBLE_ADDRESSES = [
+    '3QZWeXoFxk3Sxr2rZ7iFGLBqGuYny4PGPE',
+    '34xSpck4yJ3kjMWzaynKVFmzwY7u3KjoDC',
+    '3PtdPR38hG3PbX5bqGD5gKXmXCY9fLtFi3',
+    '3KurtNhsTjMjNCrp8PDEBZ7bpHnbh8W1sN',
+]
+# If you think any of your possible addresses must be among the first 3 addresses generated, then change this to 3
+NUM_OF_ADDRESSES_TO_GENERATE = 5    # less is faster
+
 def _create_standard_wallet(ks):
-    gap_limit = 1  # make tests run faster
     store = WalletStorage('if_this_exists_mocking_failed_648151893')
     store.put('keystore', ks.dump())
-    store.put('gap_limit', gap_limit)
+    store.put('gap_limit', NUM_OF_ADDRESSES_TO_GENERATE)
     w = Standard_Wallet(store)
     w.synchronize()
     return w
 
 def test_bip39_seed_bip49_p2sh_segwit(password):
-    # Change this to be YOUR seed phrase:
-    seed_words = 'final round trust era topic march brain envelope spoon minimum bunker start'
-    # Change this to be the address you want to generate.
-    # This example is generated with the password 12345
-    address = '3G2rhsBYmP6RKrjW1kVE1kJB9qjwggaCZw'
     # The BIP32/43 path below could be made a parameter:
-    ks = from_bip39_seed(seed_words, password, "m/49'/0'/0'")
+    ks = from_bip39_seed(SEED_WORDS, password, "m/49'/0'/0'")
     w = _create_standard_wallet(ks)
-    if (w.get_receiving_addresses()[0] == address):
-        return True
-    else:
-        return False
+    for possible_address in POSSIBLE_ADDRESSES:
+        for address in w.get_receiving_addresses():
+            if ( possible_address == address):
+                return True, address
+    return False, None
 
 def check_pass(password, failures):
-    if (test_bip39_seed_bip49_p2sh_segwit(password)):
-        print(failures + '. Found address for pw: ' + password)
+    is_found, address = test_bip39_seed_bip49_p2sh_segwit(password)
+    if (is_found):
+        print(failures + '. FOUND!\npassword: "' + password + '"\naddress: "' + address +'"')
         sys.exit(1)
     else:
         print(failures + '. NOT: ' + password)
         return False
 
 # Read passwords from STDIN and check them against known address above
-chars = ''
 failures = 1
-
-for c in sys.stdin.read():
-    if c != '\n':
-        chars += c
-    else:
-        password = chars
-        chars = ''
-        if not check_pass(password, str(failures)):
-            failures += 1
+for password in sys.stdin.read().split('\n'):
+    if not check_pass(password, str(failures)):
+        failures += 1
